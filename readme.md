c0017189f5139facd5c1ba63607458c282768ff3
feng <103472156+fengyao33@users.noreply.github.com>
2024-11-10 22:35:35 +0800

    test

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ pip freeze > requirement.txt
 pip install -r requirements.txt
 
 gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
+
+python wsgi.py
---
+++ wsgi_windows.py
@@ -0,0 +1,5 @@
+import os
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
